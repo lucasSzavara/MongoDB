99d565e065d2748ad738e8d9407a6c551907ec81
Lucas dos Santos Rodrigues Szavara <szavara.lucas.santos@gmail.com>
2018-06-25 11:22:57 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const expressMongoDb = require('express-mongo-db');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(expressMongoDb('mongodb://localhost/churros'));
+app.use(bodyParser.json());
+
+app.get('/churros', (req, res) => {
+    req.db.collection('Sabores').find().toArray((err, data) => {
+        if(err){
+            res.status(500).send('Erro ao acessar o banco de dados');
+            return;
+        }
+        res.send(data);
+    });
+});
+
+app.post('/churro', (req, res) => {
+    req.db.collection('Sabores').insert(req.body, (err) => {
+        if(err){
+            res.status(500).send('Erro ao acessar o banco de dados');
+            return;
+        }
+        res.send(req.body);
+    });
+});
+
+app.listen(3000);
